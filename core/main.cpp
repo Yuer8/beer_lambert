08e1192a1fc2851c19b2d8e8e1c0e807a2238208
Dilawar <dilawars@iitb.ac.in>
2014-08-15 18:05:09 +0530

    Added gitignore and starting main.cpp file.

---
+++ core/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Entry point of this program.
+ *
+ *
+ *        Version:  1.0
+ *        Created:  Friday 15 August 2014 06:01:48  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Here we go" << endl;
+}
