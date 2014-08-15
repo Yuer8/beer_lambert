c72627443d1bf901f676ff5d05c63b8928f58d09
Dilawar <dilawars@iitb.ac.in>
2014-08-15 18:10:25 +0530

    Added missing file. Good to go.

---
+++ core/gui.cpp
@@ -0,0 +1,19 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  gui.cpp
+ *
+ *    Description:  Gui related functions.
+ *
+ *        Version:  1.0
+ *        Created:  Friday 15 August 2014 06:08:34  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+#include <iostream>
